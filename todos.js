4b9248e654a1af6fcb62e1ae4772b1a3f6ebb36e
Ilya Livshits <ilya1200@gmail.com>
2019-10-03 13:31:42 +0300

    Add insertAndComplete & insertTwoDeleteFirst

--- todos.js
+++ todos.js
@@ -20,6 +20,18 @@ class TodosPage {
             deleteButton: {
                 locator: "div.todo>span.delete",
                 locatorType: "css"
+            },
+            checkTodoBtn: {
+                locator: "div.todo>i.fa-check-circle",
+                locatorType: "css"
+            },
+            completeTodos: {
+                locator: "div.complete>span.text",
+                locatorType: "css"
+            },
+            firstTodo:{
+                locator: ".todo:first-child",
+                locatorType: "css"
             }
         };
     }
@@ -67,8 +79,10 @@ class TodosPage {
                 console.log(" lastTodoText: " + lastTodoText)
                 if (lastTodoText !== todoText) {
                     console.log("The div was deleted")
+                }else{
+                    console.log("The div was not deleted")
                 }
-            }else{
+            } else {
                 console.log("The div was deleted")
             }
         } catch (error) {
@@ -81,12 +95,125 @@ class TodosPage {
         this.closeTodosPage();
     }
 
-    insertAndComplete(todoText) {
+    async insertAndComplete(todoText) {
+        //open Page
+        await this.openTodosPage();
+        try {
+            //Insert the todoText into the input field
+            const todoInputLocator = this.locators.todoInput;
+            await this.infraDriver.write(todoText, todoInputLocator.locatorType, todoInputLocator.locator);
+
+
+            //Click the “Add” button
+            const addButton = this.locators.addButton;
+            await this.infraDriver.clickElement(addButton.locatorType, addButton.locator);
+
+
+            //Pick last todo
+            const newTodoLocator = this.locators.lastTodo;
+            const isTodoFound = await this.infraDriver.isElementExists(newTodoLocator.locatorType, newTodoLocator.locator);
+            let newTodoElem = undefined;
+
+            if (isTodoFound) {
+                console.log("found a new div");
+                newTodoElem = await this.infraDriver.findElementBy(newTodoLocator.locatorType, newTodoLocator.locator);
+            } else {
+                console.log("Error: Can’t find a new div");
+            }
+
+            //Click the “check” (green) button
+            const checkBtnLoc = this.locators.checkTodoBtn;
+            await this.infraDriver.clickElement(checkBtnLoc.locatorType, checkBtnLoc.locator, undefined, newTodoElem);
+
+            //Validate if CHECKED
+            const completeTodosLoc = this.locators.completeTodos;
+            const completeTasksElems = await this.infraDriver.findElementListBy(completeTodosLoc.locatorType, completeTodosLoc.locator);
+
+            for (let completeTaskElm of completeTasksElems) {
+                const completeTaskText = await this.infraDriver.getTextFromElement(undefined, undefined, completeTaskElm);
+                if (completeTaskText === todoText) {
+                    console.log("the new div was checked");
+
+                    //Close Page
+                    this.closeTodosPage();
+                    return;
+                }
+            }
+            console.log("Error: the new div was NOT checked");
+        } catch (error) {
+            console.error(`TodosPage > insertAndComplete(${todoText}) ERROR:`);
+            console.error(error);
+        }
+
+
+        //Close Page
+        this.closeTodosPage();
 
     }
 
-    insertTwoDeleteFirst(todoText) {
+    async addTask(task){
+            //Insert the todoText into the input field
+            const todoInputLocator = this.locators.todoInput;
+            await this.infraDriver.write(task, todoInputLocator.locatorType, todoInputLocator.locator);
 
+            //Click the “Add” button
+            const addButton = this.locators.addButton;
+            await this.infraDriver.clickElement(addButton.locatorType, addButton.locator);
+
+
+            //Pick last todo
+            const newTodoLocator = this.locators.lastTodo;
+            const isTodoFound = await this.infraDriver.isElementExists(newTodoLocator.locatorType, newTodoLocator.locator);
+
+            if (isTodoFound) {
+                console.log("found a new div");
+            } else {
+                console.log("Error: Can’t find a new div");
+            }
+    }
+
+    async insertTwoDeleteFirst(todoText1, todoText2) {
+        //open Page
+        await this.openTodosPage();
+
+        try {
+            await this.addTask(todoText1);
+            await this.addTask(todoText2);
+        
+            const firstTodoLoc = this.locators.firstTodo;
+            const firstTodoElem = await this.infraDriver.findElementBy(firstTodoLoc.locatorType,firstTodoLoc.locator);
+            
+
+            //Click the delete (red) button no first task
+            const deleteButtonLocator = this.locators.deleteButton;
+            await this.infraDriver.clickElement(deleteButtonLocator.locatorType, deleteButtonLocator.locator, undefined, firstTodoElem);
+
+            //Check first task deleted
+            const isFirstTaskExist = await this.infraDriver.isElementExists(firstTodoLoc.locatorType,firstTodoLoc.locator)
+            if(isFirstTaskExist){
+                const firstTodoElem = await this.infraDriver.findElementBy(firstTodoLoc.locatorType,firstTodoLoc.locator);
+                const firstTodoText = await this.infraDriver.getTextFromElement(undefined,undefined,firstTodoElem);            
+                
+                if(firstTodoText !== todoText1){
+                    console.log("the first div was deleted")
+                    //Close Page
+                    this.closeTodosPage();
+                    return;
+                }
+            }
+            console.log("Error: the first div was NOT deleted");
+
+        } catch (error) {
+            console.error(`TodosPage > insertTwoDeleteFirst(${todoText1} , ${todoText2}) ERROR:`);
+            console.error(error);
+        }
+
+
+
+
+
+        //Close Page
+        this.closeTodosPage();
     }
 
     //Helpers
@@ -112,5 +239,5 @@ class TodosPage {
 }
 
 
-const tdoPg = new TodosPage();
-tdoPg.insertAndDelete("Task ABC");
+module.exports = TodosPage;
+
--- todosTest.js
+++ todosTest.js
@@ -1,1 +1,32 @@
+const TodosPage = require("./todos")
+
+async function  testInsertAndDelete(){
+    const tdoPg = new TodosPage();
+    await tdoPg.insertAndDelete("Task T1");
+}
+
+async function  testInsertAndComplete(){
+    const tdoPg = new TodosPage();
+    await tdoPg.insertAndComplete("Task T2");
+}
+
+async function  testInsertTwoDeleteFirst(){
+    const tdoPg = new TodosPage();
+    await tdoPg.insertTwoDeleteFirst("Task T1","Task T2");
+}
+
+
+async function runTests(){
+    // console.log("todosTest testInsertAndDelete()")
+    // await testInsertAndDelete();
+
+    // console.log("todosTest testInsertAndComplete()")
+    // await testInsertAndComplete();
+
+    console.log("todosTest testInsertTwoDeleteFirst()")
+    await testInsertTwoDeleteFirst();
+}
+
+runTests()
+
 
